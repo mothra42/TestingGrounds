92cdb41e6e36792b1de89ccf80473f1c87466f2b
Adam <adam.a.farmer4129@gmail.com>
2019-12-12 17:21:12 -0800

    add a c++ class for character

---
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mannequin.h"
+#include "Components/CapsuleComponent.h"
+#include "Animation/AnimInstance.h"
+#include "Camera/CameraComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Weapons/Gun.h"
+
+// Sets default values
+AMannequin::AMannequin()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create First Person Camera
+	FPCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FPCamera"));
+	FPCamera->SetupAttachment(GetCapsuleComponent());
+	FPCamera->RelativeLocation = FVector(-28.56, 11.75, 30.f);
+	FPCamera->bUsePawnControlRotation = true;
+
+	// Create First Person Mesh
+	FPMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPArms"));
+	FPMesh->SetupAttachment(FPCamera);
+	FPMesh->SetOnlyOwnerSee(true);
+	FPMesh->bCastDynamicShadow = false;
+	FPMesh->CastShadow = false;
+	FPMesh->RelativeLocation = FVector(-1.604504, -5.647609, -155.168137);
+	FPMesh->RelativeRotation = FRotator(1.899995, -19.190002, 5.200006);
+
+}
+
+// Called when the game starts or when spawned
+void AMannequin::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GunBlueprint == NULL)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Gun Blueprint"));
+		return;
+	}
+
+	Gun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	Gun->AttachToComponent(FPMesh, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	Gun->AnimInstance = FPMesh->GetAnimInstance();
+
+	if (InputComponent != NULL)
+	{
+		InputComponent->BindAction("Fire", IE_Pressed, this, &AMannequin::PullTrigger);
+	}
+}
+
+// Called every frame
+void AMannequin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AMannequin::PullTrigger()
+{
+	Gun->OnFire();
+}
+
