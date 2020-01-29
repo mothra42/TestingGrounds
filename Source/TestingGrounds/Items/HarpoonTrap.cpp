1fc632f3cd7e48d1df6605ba888ea35ed2b086b2
Adam <adam.a.farmer4129@gmail.com>
2020-01-29 15:02:34 -0800

    changes to some file names

---
+++ Source/TestingGrounds/Items/HarpoonTrap.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HarpoonTrap.h"
+
+// Sets default values
+AHarpoonTrap::AHarpoonTrap()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AHarpoonTrap::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHarpoonTrap::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
